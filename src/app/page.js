4dc1422d2276186d50578369f2b277d55fe59f83
SrAndres6 <andresrjramilo671@gmail.com>
2025-08-25 16:26:07 -0500

    cargo los ultimas modificacion de usuario.jsx y page.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
+'use client';
+
+import Usuario from '../components/Usuario';
+
 export default function Home() {
+  const usuarios = [
+    { nombre: 'Adres García', edad: 18, ciudad: 'medellin' },
+    { nombre: 'james holl', edad: 25, ciudad: 'new york' },
+    { nombre: 'María López', edad: 20, ciudad: 'Valencia' },
+  ];
+
+  const saludar = (nombre) => {
+    alert(`¡Hola, ${nombre}!`);
+  };
+
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Actividad de Props en Next.js</h1>
+      {usuarios.map((usuario, index) => (
+        <Usuario key={index} usuario={usuario} saludar={saludar} />
+      ))}
     </main>
   );
 }
